d907f6c4e818d973975e0de57d002e3e484b269a
kenneth <kokiidoki@gmail.com>
2026-05-22 09:18:09 -0400

    load bundles

--- CustomLevelsReborn/Plugin.cs
+++ CustomLevelsReborn/Plugin.cs
@@ -1,22 +1,24 @@
+using System.Collections.Generic;
+using System.IO;
 using BepInEx;
 using ComputerysModdingUtilities;
 using CustomLevelsReborn;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [assembly: StraftatMod(isVanillaCompatible: false)]
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class CLRPlugin : BaseUnityPlugin
 {
-    internal static CLRPlugin Instance;
+    internal List<string> ScenePaths = new();
 
     Harmony _harmony = new(MyPluginInfo.PLUGIN_GUID);
 
     void Awake()
     {
         _harmony.PatchAll();
-        Instance = this;
         this.gameObject.hideFlags = HideFlags.HideAndDontSave;
 
 
@@ -25,5 +27,28 @@ public class CLRPlugin : BaseUnityPlugin
     void OnDestroy()
     {
         _harmony.UnpatchSelf();
+    }
+
+    bool Debug = true; // TODO: replace with system that I won't forget to toggle
+    List<string> BundleNames = ["test_map"];
+    private void LoadBundles()
+    {
+        string bundlePath = Debug ? Path.Combine(Paths.PluginPath, "DEVELOPMENT-BUILD-Custom Levels Reborn") : Path.GetDirectoryName(Info.Location);
+
+        // var sharedAssets = AssetBundle.LoadFromFile(Path.Combine(bundlePath, "shared"));
+        // foreach (var material in sharedAssets.LoadAllAssets<Material>())
+        //     material.shader = Shader.Find(material.shader.name);
+        // SharedBundle = sharedAssets;
+        foreach (var filePath in Directory.GetFiles(bundlePath))
+        {
+            var fileName = Path.GetFileName(filePath);
+            if (!BundleNames.Contains(fileName) || fileName == "shared") continue;
+
+            var bundle = AssetBundle.LoadFromFile(Path.Combine(bundlePath, fileName));
+            foreach (var path in bundle.GetAllScenePaths())
+            {
+                ScenePaths.Add(path);
+            }
+        }
     }
 }
